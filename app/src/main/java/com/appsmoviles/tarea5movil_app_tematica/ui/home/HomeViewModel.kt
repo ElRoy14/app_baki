f7a207bcae108a9587bc9bf3ea2f5a89a312e2a0
ElRoy14 <roylinrodriguez5@gmail.com>
2024-06-12 21:55:13 -0400

    Init: Creating the Android project.

---
+++ app/src/main/java/com/appsmoviles/tarea5movil_app_tematica/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.appsmoviles.tarea5movil_app_tematica.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
