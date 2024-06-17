cff40e5f81b424de13cb17ec68bd23d7dc72b89d
ElRoy14 <roylinrodriguez5@gmail.com>
2024-06-16 23:56:52 -0400

    Video added to portada

--- app/src/main/java/com/appsmoviles/tarea5movil_app_tematica/ui/portada/PortadaFragment.kt
+++ app/src/main/java/com/appsmoviles/tarea5movil_app_tematica/ui/portada/PortadaFragment.kt
@@ -1,13 +1,18 @@
 package com.appsmoviles.tarea5movil_app_tematica.ui.portada
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.appsmoviles.tarea5movil_app_tematica.databinding.FragmentPortadaBinding
+
 
 class PortadaFragment : Fragment() {
 
@@ -17,6 +22,7 @@ class PortadaFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,10 +32,34 @@ class PortadaFragment : Fragment() {
         _binding = FragmentPortadaBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textPortada
-        textView.text = "Portada Baki App"
+        val myShader: Shader = LinearGradient(
+            0f, 50f, 0f, 100f,
+            Color.WHITE, Color.parseColor("#bc131f"),
+            Shader.TileMode.CLAMP
+        )
+
+        binding.textPortada.text = "BAKI HANMA"
+        binding.textPortada.paint.setShader(myShader)
+
+        CargarVideo()
 
         return root
+    }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun CargarVideo(){
+
+        val webView: WebView = binding.VideoPortada
+
+        val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/xniBk7zAKMY?si=2-QR6hASp2Z8eKIH\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+
+        webView.loadData(video, "text/html", "utf-8")
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.webChromeClient = WebChromeClient()
+
     }
 
     override fun onDestroyView() {
@@ -37,3 +67,4 @@ class PortadaFragment : Fragment() {
         _binding = null
     }
 }
+
