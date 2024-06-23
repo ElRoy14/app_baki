542deb4a2f2173dafb635044024b954279fa13ff
ElRoy14 <roylinrodriguez5@gmail.com>
2024-06-22 22:27:13 -0400

    App Baki completed

--- app/src/main/java/com/appsmoviles/tarea5movil_app_tematica/ui/mivida/MiVidaFragment.kt
+++ app/src/main/java/com/appsmoviles/tarea5movil_app_tematica/ui/mivida/MiVidaFragment.kt
@@ -1,11 +1,17 @@
 package com.appsmoviles.tarea5movil_app_tematica.ui.mivida
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore.Video
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.appsmoviles.tarea5movil_app_tematica.R
 import com.appsmoviles.tarea5movil_app_tematica.databinding.FragmentAcercadeBinding
 import com.appsmoviles.tarea5movil_app_tematica.databinding.FragmentMividaBinding
 import com.appsmoviles.tarea5movil_app_tematica.databinding.FragmentMomentosBinding
@@ -27,7 +33,16 @@ class MiVidaFragment : Fragment() {
         _binding = FragmentMividaBinding.inflate(inflater, container, false)
         val root : View = binding.root
 
-        
+        val video = binding.VideoMiVida
+
+        val uri: Uri = Uri.parse(
+            "android.resource://" + context?.packageName + "/" + R.raw.video__appbaki
+        )
+
+        video.setVideoURI(uri)
+        video.start()
+        video.requestFocus()
+
         return root
     }
 
